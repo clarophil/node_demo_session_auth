111c2dcc4f759ea1e23bb3ed32a483535e145ccb
Philippe DEKIMPE <dkp@ecam.be>
2023-03-28 16:02:29 +0200

    'fix'

--- server_old.js
+++ server_old.js
@@ -1,7 +1,7 @@
 let express  = require('express');
 let app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 let session =  require('express-session');
 
@@ -9,14 +9,13 @@ let user = { username:'admin', password: '1234'};
 let users = [];
 users.push(user);
 
-// Check login and password
-let check = function(req, res, next) {
-    console.log(req.session.iduser);
-    if (req.session && (req.session.iduser >=0 ) )
-      return next();
-    else
-      return res.status(401).send("Access denied !  <a href='/login_form'>Login</a>"); 
-};
+
+const logs = (req, res, next) => {
+    console.log(req.sessionID);
+    next();
+  };
+
+//app.use(logs);
 
 // Start using session
 app.use(session({
@@ -25,12 +24,52 @@ app.use(session({
     saveUninitialized: false
     })
 );
+// Check if authorized
+const check =  (req, res, next) => {
+    if (req.session && req.session.iduser >= 0 ) {
+        next();
+    }
+    else {
+        res.send('Access denied');
+    }
+};
 
-const logs = (req, res, next) => {
-    console.log(req.sessionID);
-    next();
-  };
 
+
+// Login and check user account, set session iduser
+app.get('/login', logs, (req, res, next)=> {
+    i= 0;
+    users.forEach(user => {
+        if (req.query.username == user.username && 
+            req.query.password == user.password ) {
+            req.session.iduser = i;
+            res.send('login success')
+        }
+        i++;
+    }); 
+    if ( ! (req.session.iduser >=0) ) {
+        res.send('not authorized');
+    }
+    }
+);
+
+// Logout and destroy session
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.send('logout success');
+});
+
+
+// Test redirect
+app.get('/content', check ,function (req, res) {
+    res.redirect("/content2");
+});
+
+// Get content endpoint
+app.get('/content2', function (req, res) {
+    console.log(req.session.iduser);
+    res.render("content.ejs");
+});
 
 // Send register form
 app.get('/register_form',(req, res) => {
@@ -39,6 +78,7 @@ app.get('/register_form',(req, res) => {
 
 // Save new account
 app.post('/register_save', (req, res) => {
+    console.log(req.body);
     let user = { username: req.body.username, password: req.body.password };
     users.push(user);
     console.log(users);
@@ -59,25 +99,6 @@ app.delete('/user/:iduser', (req, res) => {
     res.send('user delete'); 
 });
 
-// Login and check user account, set session iduser and cookie username
-app.get('/login',  function (req, res) {
-    i = 0;
-    console.log('query ' + req.query)
-    users.forEach(user => { 
-        i++;
-        if(req.query.username == user.username && req.query.password == user.password ) {
-            req.session.iduser = i;
-            res.cookie('username',req.query.username);
-        }
-    });
-    console.log(req.session.iduser);
-    if ( ! (req.session.iduser >= 0)  ) {
-        res.send("Login failed ! <a href='/login_form'>Try again</a> ");
-    } else {
-        res.send("login success!  <a href='/content'>Goto content</a> ");
-    }
-  });
-
 // Send login form
 app.get('/login_form',(req, res) => {
     let username = "";
@@ -87,23 +108,11 @@ app.get('/login_form',(req, res) => {
 });
 
 // Redirect to home to content
-app.get('/', logs, (req, res) => {
+app.get('/', (req, res) => {
     if (req.session.iduser >= 0) {
         res.redirect('/content');
     } else 
     res.redirect('/login_form');
-});
-
-// Logout and destroy session
-app.get('/logout', function (req, res) {
-    req.session.destroy();
-    res.clearCookie('username');
-    res.send("Logout success! ");
-    });
-
-// Get content endpoint
-app.get('/content', check, function (req, res) {
-    res.send("You can only see this after you've logged in.<br> <a href='/logout'>Logout</a> ");
 });
 
 let port = 8000;
